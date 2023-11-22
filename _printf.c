4eaec5d700cdc86bd149061f94106246a7cc1207
Remz97 <remzikeqi@icloud.com>
2023-11-22 01:59:35 -0800

    _print.c

--- _printf.c
+++ _printf.c
@@ -1,15 +1,22 @@
 #include "main.h"
 #include <stdarg.h>
-#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * _printf - function that produces output according to a format
+ * @format: format to be used to print the output
+ * Return: the number of characters printed
+ */
 
 int _printf(const char *format, ...)
 {
-	va_list ag_list;
-	int i, len = 0;
-	print_data p_func[] =
+	va_list args;
+	int i = 0;
+	int (*func)(va_list);
+	int count = 0;
+	int length;
+
+	if (format != NULL)
 	{
-		{"s", pr_string},
-		{"c", pr_char},
-		{NULL, NULL}
-	};
-}
+		length = strlen(format);
+		if ((length != 1) || (format[0] != '%'))
